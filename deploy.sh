ec6d64873f892baaf8c171f746f0bb8acac8ee12
Julian Hess <jhess@broadinstitute.org>
2020-09-14 15:19:37 -0400

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,30 @@
 #!/bin/bash
 
-docker build -t getzlab/het_pulldown_from_callstats:v1 .
+# method name
+METHOD_NAME=het_pulldown_from_callstats
 
-docker tag getzlab/het_pulldown_from_callstats:v1 gcr.io/broad-getzlab-workflows/het_pulldown_from_callstats:v1
-docker tag getzlab/het_pulldown_from_callstats:v1 gcr.io/broad-getzlab-workflows/het_pulldown_from_callstats:latest
+# docker build parameters (most likely, the defaults are OK)
+EXTRA_DOCKER_BUILD_ARGS=""
+DOCKER_BUILD_PATH="."
 
-docker push gcr.io/broad-getzlab-workflows/het_pulldown_from_callstats:v1
-docker push gcr.io/broad-getzlab-workflows/het_pulldown_from_callstats:latest
+#
+# DO NOT EDIT PAST THIS POINT UNLESS YOU HAVE VERY GOOD REASON TO DO SO
+#
+
+# automatically create method version number
+NCOMMIT=$(git rev-list --count HEAD)
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+if [[ $BRANCH == "master" ]]; then
+	BRANCH=""
+else
+	BRANCH+="_"
+fi
+VERSION=${BRANCH}v${NCOMMIT}
+
+# build method docker
+docker build -t getzlab/$METHOD_NAME:${VERSION} ${EXTRA_DOCKER_BUILD_ARGS} ${DOCKER_BUILD_PATH}
+
+# push method docker
+docker tag getzlab/$METHOD_NAME:${VERSION} gcr.io/broad-getzlab-workflows/$METHOD_NAME:${VERSION}
+docker push gcr.io/broad-getzlab-workflows/$METHOD_NAME:${VERSION}
