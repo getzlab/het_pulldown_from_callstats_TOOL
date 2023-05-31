38fe173c6e5d113c3539472a23e0716aa0f42150
Julian Hess <jhess@broadinstitute.org>
2023-05-31 15:35:44 +0000

    Bump Docker

--- wolF/tasks.py
+++ wolF/tasks.py
@@ -27,7 +27,7 @@ class get_het_coverage_from_callstats(wolf.Task):
         "normal_genotype" : "het_coverage.genotype.tsv"
     }
     resources = { "mem" : "4G" }
-    docker = "gcr.io/broad-getzlab-workflows/het_pulldown_from_callstats:tonly_v56"
+    docker = "gcr.io/broad-getzlab-workflows/het_pulldown_from_callstats:v58"
 
 class gather_het_coverage(wolf.Task):
     inputs = {
