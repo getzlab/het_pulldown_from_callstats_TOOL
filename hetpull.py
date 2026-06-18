5f67ffb2676ea5ed50cde7327c019764301798ff
Ron Solan <rsolan@broadinstitute.org>
2026-06-18 12:33:45 -0400

    Actually sorting the output...

--- hetpull.py
+++ hetpull.py
@@ -241,7 +241,7 @@ if __name__ == "__main__":
 		good_idx = (H["prob_homalt"] < 0.01) & (H["prob_homref"] < 0.1)
 
 	# BCFtools requires H to be sorted.
-	H.sort_values([CHROMOSOME, POSITION])
+	H.sort_values([CHROMOSOME, POSITION], inplace=True)
 
 	# save all possible het sites to file
 	H.to_csv(args.out_prefix + ".all_sites.tsv", sep = "\t", index = False)
